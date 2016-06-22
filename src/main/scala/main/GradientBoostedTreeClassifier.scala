c9c52f7340f62273888799046671521056718837
Christos Charisiadis <chcharisi@gmail.com>
2016-06-22 21:40:07 +0300

    Adds RandomForestClassifier and GradientBoostedTreeClassifier

---
+++ src/main/scala/main/GradientBoostedTreeClassifier.scala
@@ -0,0 +1,42 @@
+package main
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.mllib.tree.DecisionTree
+import org.apache.spark.mllib.tree.model.DecisionTreeModel
+import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.tree.impurity.Gini
+import org.apache.spark.mllib.tree.configuration.Algo._
+
+object GradientBoostedTreeClassifier {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("Simple Application").setMaster("local[4]")
+    val sc = new SparkContext(conf)
+
+    val vectors = sc.textFile("/home/christos/BigDataProject/data/train/vectors2.txt")
+
+    val parsedData = vectors.map { line =>
+      val parts = line.split(",")
+      LabeledPoint(parts(0).toDouble, Vectors.dense(parts(1).split(" ").map(_.toDouble)))
+    }
+
+    val splits = parsedData.randomSplit(Array(0.7, 0.3), seed = 11L)
+    val training = splits(0)
+    val test = splits(1)
+
+    val maxDepth = 5
+    val model = DecisionTree.train(training, Classification, Gini, maxDepth)
+
+    val predictionAndLabel = test.map(p => (model.predict(p.features), p.label))
+    val accuracy = 1.0 * predictionAndLabel.filter(x => x._1 == x._2).count() / test.count()
+
+    println (accuracy)
+
+
+    sc.stop()
+
+  }
+
+}
+
